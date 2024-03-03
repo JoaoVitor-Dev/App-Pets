2df2a51378d4968ae8ce308c2168d776eb35a9a7
JoaoVitor-dev <joaovitorreal@outlook.com>
2024-03-03 11:20:04 -0300

    Criado layout da tela de questionário

---
+++ app/src/main/java/com/example/pets/ActivityQuizGato.java
@@ -0,0 +1,14 @@
+package com.example.pets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityQuizGato extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_gato);
+    }
+}
