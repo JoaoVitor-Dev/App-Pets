2435ff593eab1e589faa59f97f6da408dafd99cd
JoaoVitor-dev <joaovitorreal@outlook.com>
2024-03-03 11:40:43 -0300

    Implementado alternativas nos questionários

--- app/src/main/java/com/example/pets/ActivityCacatua.java
+++ app/src/main/java/com/example/pets/ActivityCacatua.java
@@ -11,12 +11,15 @@ public class ActivityCacatua extends AppCompatActivity {
 
     private Button btnVoltar;
 
+    private Button btnQuis;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cacatua);
 
         btnVoltar = findViewById(R.id.btnVoltar);
+        btnQuis = findViewById(R.id.btnQuestionario);
 
         btnVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +28,15 @@ public class ActivityCacatua extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btnQuis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivityCacatua.this, ActivityQuizCacatua.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/pets/ActivityGato.java
+++ app/src/main/java/com/example/pets/ActivityGato.java
@@ -12,6 +12,8 @@ public class ActivityGato extends AppCompatActivity {
 
     private Button btnVoltar;
 
+    private Button btnQuiz;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +21,20 @@ public class ActivityGato extends AppCompatActivity {
         setContentView(R.layout.activity_gato);
 
         btnVoltar = findViewById(R.id.btnVoltarMenu);
+        btnQuiz = findViewById(R.id.btnQuestionario);
 
         btnVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(ActivityGato.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivityGato.this, ActivityQuizGato.class);
                 startActivity(intent);
             }
         });
